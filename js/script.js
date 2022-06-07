63464424b2493b78372872f4683570c12aa41242
Syzygianinfern0 <spsharan2000@gmail.com>
2022-06-07 23:31:09 +0530

    Placeholder

---
+++ js/script.js
@@ -0,0 +1,28 @@
+var r = document.querySelector(':root');
+var rs = getComputedStyle(r);
+const button = document.querySelector(".toggle-btn");
+button.addEventListener("click", toggle);
+
+if (localStorage.getItem("al") === null && localStorage.getItem("bg") === null && localStorage.getItem("fg") === null) {
+    bg = rs.getPropertyValue('--bg');
+    fg = rs.getPropertyValue('--fg');
+} else {
+    bg = localStorage.getItem("bg");
+    fg = localStorage.getItem("fg");
+    load();
+}
+
+function toggle() {
+    var t = document.querySelector(".toggle");
+    bg = rs.getPropertyValue('--bg');
+    fg = rs.getPropertyValue('--fg');
+    r.style.setProperty('--bg', fg);
+    r.style.setProperty('--fg', bg);
+    localStorage.setItem("bg", fg);
+    localStorage.setItem("fg", bg);
+}
+
+function load() {
+    r.style.setProperty('--bg', bg);
+    r.style.setProperty('--fg', fg);
+}
